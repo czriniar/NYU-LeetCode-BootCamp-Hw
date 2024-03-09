69db3f0beb01315731b977a16e8bf7fe5b771b6b
David Ren <150967374+czriniar@users.noreply.github.com>
2024-03-08 20:22:58 -0500

    Update strtointaoi.py

--- hw2/strtointaoi.py
+++ hw2/strtointaoi.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        nums = '0123456789'
+        symbols = '-'
+        res_str = ''
+        can_zero = False
+        if s[0] not in nums and s[0] not in symbols and s[0] != ' ':
+                return 0
+        for c in s:
+            if c == '0' and can_zero:
+                res_str += c
+            if c in nums or c in symbols and c != '0':
+                res_str += c
+                can_zero = True
+        integer = int(res_str)
+        return integer
